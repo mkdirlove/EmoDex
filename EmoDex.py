c847acc85dfb64278976e50c48539d26040105f5
Jayson Cabrillas San Buenaventura <52001009+mkdirlove@users.noreply.github.com>
2023-02-17 04:27:02 +0000

    Patch #1
    -Added a function to detect the language of the user input.
    -Added a function that can translate the detected language to English.

--- EmoDex.py
+++ EmoDex.py
@@ -12,35 +12,57 @@ banner = """
                Made with <3 by @mkdirlove
 """
 
-os.system("clear")
-print(banner)
-parser = argparse.ArgumentParser(description='EmoDex - An AI-based text / emotion analysis tool.')
-parser.add_argument("-s", "--sentence", help="Input sentence to analyze emotions", required=True)
-args = parser.parse_args()
+def translate_to_english(text):
+    lang = detect(text)
+    if lang != 'en':
+        translator = Translator(to_lang='en', from_lang=lang)
+        return translator.translate(text)
+    else:
+        return text
 
+def analyze_emotions(sentence):
+    sentence = translate_to_english(sentence)
 
-if not args.sentence:
-    parser.print_help()
-else:
     url = "https://emodex-emotions-analysis.p.rapidapi.com/rapidapi/emotions"
 
-    payload = {"sentence": args.sentence}
+    payload = {"sentence": sentence}
     headers = {
-    "content-type": "application/json",
-    "X-RapidAPI-Key": "18140c0733msh4177590f10ca716p1ba0c8jsnd344b761be19",
-    "X-RapidAPI-Host": "emodex-emotions-analysis.p.rapidapi.com"
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "18140c0733msh4177590f10ca716p1ba0c8jsnd344b761be19",
+        "X-RapidAPI-Host": "emodex-emotions-analysis.p.rapidapi.com"
     }
 
-response = requests.request("POST", url, json=payload, headers=headers)
+    response = requests.request("POST", url, json=payload, headers=headers)
 
-data = response.json()
-print("Sentence: ", data["sentence"]["text"])
-print()
-print("Anger: ", data["sentence"]["anger"])
-print("Disgust: ", data["sentence"]["disgust"])
-print("Fear: ", data["sentence"]["fear"])
-print("Joy: ", data["sentence"]["joy"])
-print("Love: ", data["sentence"]["love"])
-print("No Emotion: ", data["sentence"]["noemo"])
-print("Sadness: ", data["sentence"]["sadness"])
-print("Surprise: ", data["sentence"]["surprise"])
+    data = response.json()
+    print("Sentence: ", data["sentence"]["text"])
+    print()
+    print("Anger: ", data["sentence"]["anger"])
+    print("Disgust: ", data["sentence"]["disgust"])
+    print("Fear: ", data["sentence"]["fear"])
+    print("Joy: ", data["sentence"]["joy"])
+    print("Love: ", data["sentence"]["love"])
+    print("No Emotion: ", data["sentence"]["noemo"])
+    print("Sadness: ", data["sentence"]["sadness"])
+    print("Surprise: ", data["sentence"]["surprise"])
+
+def main():
+    os.system("clear")
+    print(banner)
+    parser = argparse.ArgumentParser(description='EmoDex - An AI-based text / emotion analysis tool.')
+    parser.add_argument("-s", "--sentence", help="Input sentence to analyze emotions", required=True)
+    args = parser.parse_args()
+
+    if not args.sentence:
+        parser.print_help()
+    else:
+        analyze_emotions(args.sentence)
+
+if __name__ == '__main__':
+    try:
+        from langdetect import detect
+        from translate import Translator
+    except ImportError:
+        print("langdetect and translate is not installed. Installing it now...")
+        os.system("python3 -m pip install langdetect translate")
+    main()
